e271d4f51b47e968b68a413bd96c3fa4df1c4218
AbsoluteZer0 <ddmitrij2@yandex.ru>
2024-09-23 00:08:43 +0300

    update 0.4 добавил: панель администратора создал: суперюзера и настроил панель администратора проверил: работоспособность админ панели и отдельно запросов на создание, изменение и поиск по фильтру, через консоль

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Game)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'cost',)
+    search_fields = ('name',)
+    list_filter = ('cost', 'size',)
+
+
+admin.site.register(Post)
